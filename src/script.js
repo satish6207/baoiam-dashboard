1553ce9ff696924d81ddff752f6c9e09d5086921
GOVINDU SATISH <121442641+satish6207@users.noreply.github.com>
2023-11-29 19:41:03 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
   
-  
     document.addEventListener("DOMContentLoaded", function () {
         const menuIcon = document.getElementById("menuIcon");
         const sidebar = document.getElementById("sidebar");
@@ -32,7 +31,25 @@
             closebtn.style.display = "block";
             content.style.width = "calc(100% - 250px)";
             content.style.left = "250px";
+            sidebar.onfullscreenchange.display = "block";
         }
     }
+    // document.addEventListener("fullscreenchange", function () {
+    //     const sidebar = document.getElementById("sidebar");
+    //     const content = document.getElementById("content");
+    //     const closeIcon = document.getElementById("close");
+    
+    //     if (document.fullscreenElement) {
+    //         sidebar.style.display = "block";
+    //         // closeIcon.style.display = "block";
+    //         content.style.width = "calc(100% - 250px)";
+    //         content.style.left = "250px";
+    //     } else {
+    //         sidebar.style.display = "none";
+    //         // closeIcon.style.display = "none";
+    //         content.style.width = "100%";
+    //         content.style.left = "0";
+    //     }
+    // });
     
     
