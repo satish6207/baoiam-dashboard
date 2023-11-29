2c1fec3e1f597e2decf1ecd180bc72fc7d96e573
GOVINDU SATISH <121442641+satish6207@users.noreply.github.com>
2023-11-29 18:34:14 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,11 +1,38 @@
-
-    function toggleSidebar() {
-        var content = document.getElementById("content");
-        var sidebar = document.getElementById("sidebar");
-
-        sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
-
-        content.style.display = sidebar.style.display === "block" ? "none" : "block";  
+  
+  
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuIcon = document.getElementById("menuIcon");
+        const sidebar = document.getElementById("sidebar");
+    
+        function toggleSidebar() {
+            sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
+        }
+        menuIcon.addEventListener("click", toggleSidebar);
+    });
+    
+    function closeSidebar() {
+        const sidebar = document.getElementById("sidebar");
+        const content = document.getElementById("content");
+    
+        sidebar.style.display = "none";
+        content.style.width = "100%";
+        content.style.left = "0";
     }
-
+    function toggleSidebar() {
+        const sidebar = document.getElementById("sidebar");
+        const content = document.getElementById("content");
+        const closebtn = document.getElementById("close");
+    
+        sidebar.style.display = sidebar.style.display === "none" ? "block" : "none";
+    
+        if (sidebar.style.display === "none") {
+            content.style.width = "100%";
+            content.style.left = "0";
+        } else {
+            closebtn.style.display = "block";
+            content.style.width = "calc(100% - 250px)";
+            content.style.left = "250px";
+        }
+    }
+    
     
